601ccd9898b1ccbf5e07efa8e96f32c987f1739a
YeahNahCunt <42754470+YeahNahCunt@users.noreply.github.com>
2019-11-05 22:58:23 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,10 +6,11 @@ client = commands.Bot(command_prefix = '|') #holder prefix change later
 
 @client.event
 async def on_ready():
+    await client.change_presence(status=discord.Status.online, activity=discord.Game('Suck ya mum')
     print('Bot is ready!')
-    
-@client.command() #for testing host
+
+@client.command()
 async def ping(ctx):
-    await ctx.send(f'Surprise motherfucker in {client.latency * 1000}ms !') 
+    await ctx.send(f'Pong! {client.latency * 1000}ms')
 
 client.run(os.environ['BOT_TOKEN'])
