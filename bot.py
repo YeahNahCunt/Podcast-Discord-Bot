e729219238815f50f54a95011f1d9a270957a68f
YeahNahCunt <42754470+YeahNahCunt@users.noreply.github.com>
2019-11-05 22:13:12 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,11 @@
+import discord
+from discord.ext import commands
+import os #for bot token linking with host
 
+client = commands.Bot(command_prefix = '|') #holder prefix change later
+
+@client.event
+async def on_ready():
+    print('Bot is ready!')
+
+client.run(os.environ['BOT_TOKEN'])
