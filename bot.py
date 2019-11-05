6d0177c38717ad8c0a1ce1d89ad5e0ddae955c8c
YeahNahCunt <42754470+YeahNahCunt@users.noreply.github.com>
2019-11-05 23:30:22 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ import os #for bot token linking with host
 
 client = commands.Bot(command_prefix = '|') #holder prefix change later
 
-@client.event
+@client.event()
 async def on_ready():
     await client.change_presence(status=discord.Status.online, activity=discord.Game('Suck ya mum')
     print('Bot is ready!')
