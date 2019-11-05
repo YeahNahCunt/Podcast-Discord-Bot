14ea44fca5f19041a470cec99c2448f6c8052e16
YeahNahCunt <42754470+YeahNahCunt@users.noreply.github.com>
2019-11-05 23:24:16 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,10 +6,7 @@ client = commands.Bot(command_prefix = '|') #holder prefix change later
 
 @client.event
 async def on_ready():
-    print('Bot is ready!') 
-
-async def on_ready():
-    await client.change_presence(status=discord.Status.online, activity=discord.Game('Suck ya mum')
+    print('Bot is ready!')
 
 @client.command()
 async def ping(ctx):
